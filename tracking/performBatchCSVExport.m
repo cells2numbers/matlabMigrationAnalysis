e1cf77506cf732ad20559503cc2f71e51cf1db20
Tim Becker <tbecker@broadinstitute.org>
2017-03-29 10:16:38 -0400

    added csv export, solves #3

--- tracking/performBatchCSVExport.m
+++ tracking/performBatchCSVExport.m
@@ -1,8 +1,19 @@
-function [] = performBatchTracking(expPath,OVERWRITE)
+function [] = performBatchCSVExport(expPath,OVERWRITE)
+% Batch csv export as tidy list 
 %
+% All analyzed experiments found in the given folder are exported as 
+% csv
 %
+% Input
+%   expPath         The experiment path is recursively searched for 
+%                   analyzed experiments
 %
+% Optional Input
+%   OVERWRITE       Force overwrite of already exported experiments    
 % 
+%
+% See also exportTidyTrajectories, correctFM, performCSVExport
+% 3.2017 tb
 
 if ~exist('OVERWRITE','var')
     OVERWRITE = 0;
@@ -11,13 +22,7 @@ end
 filename = 'expInfo.mat';
 [fileList,pathList] = searchFileRec(expPath,filename);
 
-
 nFiles = size(fileList,2);
-
-radius = 45;
-frameRadius = 3;
-SAVE = 1;
-detection_method = 2;
 
 for i=1:nFiles
     % check if detection file is written
@@ -30,28 +35,21 @@ end
 fprintf('\nfound %i extracted experiment(s) \n ',nFiles);
 fprintf(' parameter stored: %i \n ',length(find(exp.detectionParaStored)));  
 fprintf(' already tracked:  %i \n ',length(find(exp.tracked)));
-fprintf(' already analyzed: %i \n',length(find(exp.analyzed)));
+fprintf(' already analyzed: %i \n ',length(find(exp.analyzed)));
 fprintf(' already exported: %i \n ',length(find(exp.exported)));
-%%
 
 if OVERWRITE
-     index2track = find(exp.detectionParaStored);
+     index2export = find(exp.analyzed);
 else 
-    index2track = setdiff(find(exp.detectionParaStored), find(exp.tracked));
+    index2export = setdiff(find(exp.analyzed), find(exp.exported));
 end
 
-if length(index2track) == 0
+if isempty(index2export)
     fprintf('I am bored, nothing to do. Bye... \n');
 else
-    
-    parfor i=1:length(index2track)
-        fprintf('---------------------------------------------------\n');
-        fprintf('tracking image series %i of the last missing %i \n',i,length(index2track));
-        iExp = index2track(i);
-        performTracking(pathList{iExp},'image_001.png',...
-            detection_method,radius,frameRadius,SAVE);
-        fprintf('finished image series %i of the last missing %i \n',i,length(index2track));
-        fprintf('---------------------------------------------------\n\n');
+    parfor i=1:length(index2export)
+        fprintf('exporting image series %i / %i \n',i,length(index2export));
+        iExp = index2export(i);
+        performCSVExport(pathList{iExp});
     end
 end
-
